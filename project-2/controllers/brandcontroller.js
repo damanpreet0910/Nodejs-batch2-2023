b7fb866f9659bf0ff7725ae3519f04f9bed275a2
damanpreet kaur <o7services003@gmail.com>
2023-04-27 12:45:44 +0530

    block/unblock, skip,sort,limit

--- project-2/controllers/brandcontroller.js
+++ project-2/controllers/brandcontroller.js
@@ -50,6 +50,94 @@ addbrand = (req,res)=>{
     }
 }
 
+getallbrand = (req,res)=>{
+
+    limit_num = 3
+    skip_count = 0
+    if(req.body.pageno > 1)
+    {
+        skip_count = (req.body.pageno - 1)*limit_num
+        // console.log("Page found")
+    }
+
+    // console.log(skip_count)
+
+    Brand.find()
+    // .sort({_id:-1})
+    .skip(skip_count)
+    .limit(limit_num)
+    .exec()
+    .then(branddata=>{
+        res.json({
+            'status':200,
+            'success':true,
+            'msg':'data loaded',
+            'data':branddata
+        })
+    })
+    .catch(err=>{
+        res.json({
+            status:500,
+            success:false,
+            msg : 'Error Occur',
+            error : String(err)
+        })
+    })
+    
+}
+
+enabledisablebrand = (req,res)=>{
+    var validation = ""
+    if(req.body._id == "")
+    {
+        validation += "ID is required \n"
+    }
+    if(req.body.isblocked == "")
+    {
+        validation += "Status is required \n"
+    }
+
+    if(!!validation)
+    {
+        res.json({
+            status:409,
+            success:false,
+            msg:validation
+        })
+    }
+    else{
+        //check whether data exists or not wrt particular id
+        Brand.findOne({_id:req.body._id})
+        .then(branddata=>{
+            if(branddata == null)
+            {
+                res.json({
+                    status:409,success:false,msg:'Data not found'
+                })
+            }
+            else{
+                //enable disable brand 
+                branddata.isblocked = req.body.isblocked
+                branddata.save()
+
+                res.json({
+                    status:200,success:true,msg:'Record status changed'
+                })
+            }
+        })
+        .catch(err=>{
+            res.json({
+                status:500,
+                success:false,
+                msg:'Error',
+                error:String(err)
+            })
+        }) 
+    }
+}
+
 module.exports = {
-    addbrand
+    addbrand,
+    getallbrand,
+    enabledisablebrand
 }
--- project-2/routes/adminroutes.js
+++ project-2/routes/adminroutes.js
@@ -4,14 +4,19 @@ const categorycontroller = require('../controllers/categorycontroller')
 const brandcontroller = require('../controllers/brandcontroller')
 const ordercontroller = require('../controllers/orderController')
 
+//category routes start
 router.post("/addcategory",categorycontroller.addcategory)
 router.post("/getallcategory",categorycontroller.getallcategory)
 router.post("/getsinglecategory",categorycontroller.getsingle)
 router.post("/updatecategory",categorycontroller.update)
 router.post("/deletecategory",categorycontroller.deletedata)
+//category routes end
 
-
+//brand routes start
 router.post("/addbrand",brandcontroller.addbrand)
+router.post("/getallbrand",brandcontroller.getallbrand)
+router.post("/enabledisablebrand",brandcontroller.enabledisablebrand)
+
 
 router.post("/placeorder",ordercontroller.add)
 router.post("/getallorder",ordercontroller.getall)
